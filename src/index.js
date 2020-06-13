6ff4b4b578d701885224a4cce062dd97923484e6
mlanorero <62410711+mlanorero@users.noreply.github.com>
2020-06-13 13:43:46 -0400

    Simple Counter

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const SecondsCounter = () => {
+    const [seconds, setSeconds] = useState(0);
+  
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setSeconds(seconds => seconds + 1);
+      }, 1000);
+      return () => clearInterval(interval);
+    }, []);
+  
+    return (
+        <div className="text-center mt-5">
+        <div className="container d-flex justify-content-between">
+            <p className="h1 m-2 text-light">
+                <i className="far fa-clock" />
+            </p>
+            <p id="time" className="h1 mr-2 text-light">
+                {seconds}
+            </p>
+        </div>
+ </div>
+    );
+  };
+  
+ReactDOM.render(<SecondsCounter />, document.querySelector('#root'));
+
+  
